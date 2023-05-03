800dde7749fe4cfa555510061bac4084c5cf6acd
Anton Gursky <a.gursky@gmail.com>
2023-05-03 20:13:16 +0300

    Task_33 added

---
+++ src/main/java/task_33/Hello.java
@@ -0,0 +1,14 @@
+package task_33;
+
+public class Hello {
+
+    private String helloMessage;
+
+    public void getHelloMessage() {
+        System.out.println("Hello message: " + helloMessage);
+    }
+
+    public void setHelloMessage(String helloMessage) {
+        this.helloMessage = helloMessage;
+    }
+}
